4d30b007b95546bc018f5e051d60b6404dd17267
CodePorting <info@codeporting.com>
2012-07-03 02:37:54 -0700

    Auto port from CodePorting

--- PortedCode/Proj2_Initialized/Test.java
+++ PortedCode/Proj2_Initialized/Test.java
@@ -21,8 +21,7 @@ package CodePortingCSharp2Java;
      */
   public String text= "new text here";
   public static String Token = "";   
-  private int x = 200;
-  public int count= 100;   
+
            		
 }
 
